ef497d12de90874805e9b54d16293fa6c72270e2
willR30 <willycaceres2002@gmail.com>
2023-02-09 10:28:21 -1200

    categories view was added

--- index.py
+++ index.py
@@ -12,6 +12,11 @@ def home():
     return render_template('home.html', data=data)
     #return render_template('layout.html')
 
+@app.route('/categories')
+def categories():
+    data=getCategories()
+    return render_template('categories.html',data=data)
+
 
 def getProducts():
     api_url='https://api.escuelajs.co/api/v1/products'
@@ -20,7 +25,16 @@ def getProducts():
         'products': response
     }
     return data
-    
+
+
+def getCategories():
+    api_url='https://api.escuelajs.co/api/v1/categories'
+    response=requests.get(api_url).json()
+    data={
+        'categories': response
+    }
+
+    return data
 
 
 if __name__ == '__main__':
